886bb540c8a3c46b8ca51b9bd0ef52d39e7c82fb
WinteryOS <54219355+WinteryOS@users.noreply.github.com>
2021-08-24 14:14:07 -0600

    adding image

---
+++ SlotMachine/slots.js
@@ -0,0 +1,26 @@
+
+
+// (function($) {
+//     $(function() { //on DOM ready
+//         $("#scroller2").simplyScroll({
+//             customClass: 'vert',
+//             orientation: 'vertical',
+//             auto: true,
+//             manualMode: 'end',
+//             frameRate: 24,
+//             speed: 12
+//         });
+//     });
+// })(jQuery);
+// (function($) {
+//     $(function() { //on DOM ready
+//         $("#scroller3").simplyScroll({
+//             customClass: 'vert',
+//             orientation: 'vertical',
+//             auto: true,
+//             manualMode: 'end',
+//             frameRate: 24,
+//             speed: 30
+//         });
+//     });
+// })(jQuery);
